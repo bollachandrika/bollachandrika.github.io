663c4b2749c65713cf6a45f006ab778185c973a2
chandrika.bolla <chandrikabolla999@gmail.com>
2018-01-31 01:12:25 -0500

    bundles added

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
